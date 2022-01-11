1a8117f85d088b90a81bc93bade92eb9839357ae
harmolipi <nikob381@gmail.com>
2022-01-11 15:09:39 -0500

    Created input component

---
+++ src/components/MarkdownInput.js
@@ -0,0 +1,16 @@
+const MarkdownInput = ({ markdown, handleChange }) => {
+  return (
+    <div className="container mx-auto">
+      <h2>Editor</h2>
+      <textarea
+        id="editor"
+        className="w-full px-3 py-2 text-gray-700 border rounded-lg focus:outline-none h-5/6"
+        placeholder="Enter some markdown here..."
+        onChange={handleChange}
+        value={markdown}
+      />
+    </div>
+  );
+};
+
+export default MarkdownInput;
