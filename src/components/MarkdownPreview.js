b02e3b4b4623fdb30f34e7812555adef810f366a
harmolipi <nikob381@gmail.com>
2022-01-11 16:21:13 -0500

    Set marked breaks option

--- src/components/MarkdownPreview.js
+++ src/components/MarkdownPreview.js
@@ -1,14 +1,17 @@
+import { marked } from 'marked';
+
 const MarkdownPreview = ({ previewText }) => {
+  marked.setOptions({ breaks: true });
+
   return (
     <div className="container mx-auto">
       <h2>Preview</h2>
       <div
         id="preview"
-        className="w-full px-3 py-2 text-gray-700 border rounded-lg focus:outline-none overflow-scroll h-5/6"
+        className="prose w-full px-3 py-2 text-gray-700 border rounded-lg focus:outline-none overflow-scroll h-5/6"
         rows="6"
-      >
-        {previewText}
-      </div>
+        dangerouslySetInnerHTML={{ __html: marked.parse(previewText) }}
+      ></div>
     </div>
   );
 };
